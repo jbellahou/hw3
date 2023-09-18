cfe8bf054d5659951cb09543702f294b57fbbd81
jbellahou <56601030+jbellahou@users.noreply.github.com>
2023-09-18 14:28:23 -0500

    Update util-db.php

--- util-db.php
+++ util-db.php
@@ -1,7 +1,7 @@
 <?php
 function get_db_connection(){
     // Create connection
-    $conn = new mysqli('159.89.47.44', 'projecto_hw3user', 'L.xwzZO2fH[P', 'projcto_hw3');
+    $conn = new mysqli('159.89.47.44', 'projecto_hw3user', 'L.xwzZO2fH[P', 'projecto_hw3');
     
     // Check connection
     if ($conn->connect_error) {
