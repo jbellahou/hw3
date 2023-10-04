9643af32ec0ab0b2795c3dff0cc47de3e924b1c1
jbellahou <56601030+jbellahou@users.noreply.github.com>
2023-10-04 13:46:25 -0500

    Update model-courses.php

--- model-courses.php
+++ model-courses.php
@@ -12,4 +12,46 @@ function selectCourses() {
         throw $e;
     }
 }
+
+function insertCourse($cNumber, $cDesc) {
+    try {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare("INSERT INTO `course` (`course_number`, `course_description`) VALUES (?, ?)");
+        $stmt->bind_param("ss", $cNumber, $cDesc);
+        $success = $stmt->execute();
+        $conn->close();
+        return $success;
+    } catch (Exception $e) {
+        $conn->close();
+        throw $e;
+    }
+}
+
+function updateCourse($cNumber, $cDesc, $cid) {
+    try {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare("update `course` set `course_number` = ?, `course_description` = ? where course_id = ?");
+        $stmt->bind_param("ssi", $cNumber, $cDesc, $cid);
+        $success = $stmt->execute();
+        $conn->close();
+        return $success;
+    } catch (Exception $e) {
+        $conn->close();
+        throw $e;
+    }
+}
+
+function deleteCourse($cid) {
+    try {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare("delete from course where course_id=?");
+        $stmt->bind_param("i", $cid);
+        $success = $stmt->execute();
+        $conn->close();
+        return $success;
+    } catch (Exception $e) {
+        $conn->close();
+        throw $e;
+    }
+}
 ?>
