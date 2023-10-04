0bb9d6e33e06e9716634e8dc2a9b25646cd6af01
jbellahou <56601030+jbellahou@users.noreply.github.com>
2023-10-04 14:11:39 -0500

    Update courses.php

--- courses.php
+++ courses.php
@@ -4,6 +4,15 @@ require_once("model-courses.php");
 
 $pageTitle = "Courses";
 include "view-header.php";
+
+if (isset($_POST['actionType'])) {
+  switch ($_POST['actionType']) {
+    case "Add":
+      insertCourse($_POST['cNumber'], $_POST['cDesc']);
+      break;
+  }
+}
+
 $courses = selectCourses();
 include "view-courses.php";
 include "view-footer.php";
