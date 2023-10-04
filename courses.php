3df802e09d3a9ab78b353a0d9c6b9f89f59048a3
jbellahou <56601030+jbellahou@users.noreply.github.com>
2023-10-04 14:14:50 -0500

    Update courses.php

--- courses.php
+++ courses.php
@@ -8,7 +8,11 @@ include "view-header.php";
 if (isset($_POST['actionType'])) {
   switch ($_POST['actionType']) {
     case "Add":
-      insertCourse($_POST['cNumber'], $_POST['cDesc']);
+      if (insertCourse($_POST['cNumber'], $_POST['cDesc'])) {
+        echo '<div class="alert alert-success" role="alert">Course added.</div>"';
+      } else {
+        echo '<div class="alert alert-danger" role="alert">Error.</div>"';
+      }
       break;
   }
 }
