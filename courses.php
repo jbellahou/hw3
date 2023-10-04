1d7236e41d96fb204f697080512a8d00ee021831
jbellahou <56601030+jbellahou@users.noreply.github.com>
2023-10-04 14:32:54 -0500

    Update courses.php

--- courses.php
+++ courses.php
@@ -14,6 +14,13 @@ if (isset($_POST['actionType'])) {
         echo '<div class="alert alert-danger" role="alert">Error.</div>';
       }
       break;
+    case "Edit":
+      if (updateCourse($_POST['cNumber'], $_POST['cDesc'], $_POST['cid'])) {
+        echo '<div class="alert alert-success" role="alert">Course edited.</div>';
+      } else {
+        echo '<div class="alert alert-danger" role="alert">Error.</div>';
+      }
+      break;
     case "Delete":
       if (deleteCourse($_POST['cid'])) {
         echo '<div class="alert alert-success" role="alert">Course deleted.</div>';
