a38991f3148eb87eb8ea91024b449a9319be700b
jbellahou <56601030+jbellahou@users.noreply.github.com>
2023-10-16 10:15:27 -0500

    Update view-instructors-with-courses.php

--- view-instructors-with-courses.php
+++ view-instructors-with-courses.php
@@ -1,4 +1,13 @@
+<div class="row">
+  <div class="col">
 <h1>Instructors with courses</h1>
+  </div>
+  <div class="col-auto">
+<?php
+include "view-instructors-with-courses-newform.php";
+?>
+  </div>
+</div>
 <div class="card-group">
 <?php
 while ($instructor = $instructors->fetch_assoc()) {
@@ -12,7 +21,16 @@ while ($instructor = $instructors->fetch_assoc()) {
   $courses = selectCoursesByInstructor($instructor['instructor_id']);
   while ($course = $courses->fetch_assoc()) {
 ?>
-    <li class="list-group-item"><?php echo $course['course_number']; ?> - <?php echo $course['semester']; ?> - <?php echo $course['room']; ?> - <?php echo $course['day_time']; ?></li>
+    <div class="row">
+      <div class="col">
+        <li class="list-group-item"><?php echo $course['course_number']; ?> - <?php echo $course['semester']; ?> - <?php echo $course['room']; ?> - <?php echo $course['day_time']; ?></li>
+      </div>
+      <div class="col-auto">
+<?php
+include "view-instructors-with-courses-editform.php";
+?>
+      </div>
+    </div>
 <?php
   }
 ?>
