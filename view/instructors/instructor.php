68fffcdb60c950a59367458878d3c8b3e71e0a12
jbellahou <56601030+jbellahou@users.noreply.github.com>
2023-11-08 11:31:07 -0600

    Create instructor.php

---
+++ view/instructors/instructor.php
@@ -0,0 +1,6 @@
+  <tr>
+    <td><?php echo $instructor['instructor_id']; ?></td>
+    <td><?php echo $instructor['instructor_name']; ?></td>
+    <td><?php echo $instructor['office_number']; ?></td>
+    <td><a href="courses-by-instructor.php?id=<?php echo $instructor['instructor_id']; ?>">Courses</a></td>
+  </tr>
