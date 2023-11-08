3cfa769051238693daa905a8073ea13f520facd8
jbellahou <56601030+jbellahou@users.noreply.github.com>
2023-11-08 11:32:23 -0600

    Create section.php

---
+++ view/sections-by-course/section.php
@@ -0,0 +1,8 @@
+  <tr>
+    <td><?php echo $section['course_id']; ?></td>
+    <td><?php echo $section['course_number']; ?></td>
+    <td><?php echo $section['course_description']; ?></td>
+    <td><?php echo $section['semester']; ?></td>
+    <td><?php echo $section['room']; ?></td>
+    <td><?php echo $section['day_time']; ?></td>
+  </tr>
