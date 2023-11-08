5ec431a9b2dbf83ab796ace4b1df489d14e11da9
jbellahou <56601030+jbellahou@users.noreply.github.com>
2023-11-08 11:31:54 -0600

    Update and rename view-sections-by-course.php to view/sections-by-course/page.php

--- view/sections-by-course/page.php
+++ view/sections-by-course/page.php
@@ -14,16 +14,7 @@
     <tbody>
 <?php
 while ($section = $sections->fetch_assoc()) {
-?>
-  <tr>
-    <td><?php echo $section['course_id']; ?></td>
-    <td><?php echo $section['course_number']; ?></td>
-    <td><?php echo $section['course_description']; ?></td>
-    <td><?php echo $section['semester']; ?></td>
-    <td><?php echo $section['room']; ?></td>
-    <td><?php echo $section['day_time']; ?></td>
-  </tr>
-<?php
+  include "section.php";
 }
 ?>
     </tbody>
