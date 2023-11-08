b646ad5af9b60c890ff4fc19057fcbd1926c4e64
jbellahou <56601030+jbellahou@users.noreply.github.com>
2023-11-08 11:30:22 -0600

    Update and rename view-instructors.php to view/instructors/page.php

--- view/instructors/page.php
+++ view/instructors/page.php
@@ -12,14 +12,7 @@
     <tbody>
 <?php
 while ($instructor = $instructors->fetch_assoc()) {
-?>
-  <tr>
-    <td><?php echo $instructor['instructor_id']; ?></td>
-    <td><?php echo $instructor['instructor_name']; ?></td>
-    <td><?php echo $instructor['office_number']; ?></td>
-    <td><a href="courses-by-instructor.php?id=<?php echo $instructor['instructor_id']; ?>">Courses</a></td>
-  </tr>
-<?php
+  include "instructor.php";
 }
 ?>
     </tbody>
