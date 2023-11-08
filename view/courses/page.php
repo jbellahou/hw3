f762bff2cb00579ad1c1646f8b90f7ec4660c0ad
jbellahou <56601030+jbellahou@users.noreply.github.com>
2023-11-08 11:25:04 -0600

    Update and rename view-courses.php to view/courses/page.php

---
+++ view/courses/page.php
@@ -0,0 +1,31 @@
+<div class="row">
+  <div class="col">
+<h1>Courses</h1>
+  </div>
+  <div class="col-auto">
+<?php
+include "view-courses-newform.php";
+?>
+  </div>
+</div>
+<div class="table-responsive">
+  <table class="table">
+    <thead>
+      <tr>
+      <th>ID</th>
+      <th>Number</th>
+      <th>Description</th>
+        <th></th>
+        <th></th>
+        <th></th>
+      </tr>
+    </thead>
+    <tbody>
+<?php
+while ($course = $courses->fetch_assoc()) {
+  include "course.php";
+}
+?>
+    </tbody>
+  </table>
+</div>
