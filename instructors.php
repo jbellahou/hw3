5a0ac55d62527a4f2b9caf1ae845e7ff6b11a454
jbellahou <56601030+jbellahou@users.noreply.github.com>
2023-11-08 11:46:25 -0600

    Update instructors.php

--- instructors.php
+++ instructors.php
@@ -5,6 +5,6 @@ require_once("model/instructors-db.php");
 $pageTitle = "Instructors";
 include "view/header.php";
 $instructors = selectInstructors();
-include "view/instructors-db.php";
+include "view/instructors/page.php";
 include "view/footer.php";
 ?>
