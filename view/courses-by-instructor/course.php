203a11d544e713d87e83f4b124b7a02f878b8965
jbellahou <56601030+jbellahou@users.noreply.github.com>
2023-11-08 11:23:02 -0600

    Create course.php

---
+++ view/courses-by-instructor/course.php
@@ -0,0 +1,8 @@
+  <tr>
+    <td><?php echo $course['course_id']; ?></td>
+    <td><?php echo $course['course_number']; ?></td>
+    <td><?php echo $course['course_description']; ?></td>
+    <td><?php echo $course['semester']; ?></td>
+    <td><?php echo $course['room']; ?></td>
+    <td><?php echo $course['day_time']; ?></td>
+  </tr>
