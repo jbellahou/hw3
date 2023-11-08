d21edbd23cf6e049b33b8e515cd7851e2a69aa6c
jbellahou <56601030+jbellahou@users.noreply.github.com>
2023-11-08 11:22:00 -0600

    Update and rename view-courses-by-instructor.php to view/courses-by-instructor/page.php

--- view/courses-by-instructor/page.php
+++ view/courses-by-instructor/page.php
@@ -14,16 +14,7 @@
     <tbody>
 <?php
 while ($course = $courses->fetch_assoc()) {
-?>
-  <tr>
-    <td><?php echo $course['course_id']; ?></td>
-    <td><?php echo $course['course_number']; ?></td>
-    <td><?php echo $course['course_description']; ?></td>
-    <td><?php echo $course['semester']; ?></td>
-    <td><?php echo $course['room']; ?></td>
-    <td><?php echo $course['day_time']; ?></td>
-  </tr>
-<?php
+  include "course.php";
 }
 ?>
     </tbody>
